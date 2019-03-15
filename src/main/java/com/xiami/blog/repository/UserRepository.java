802b13ba34d97f98a357966343cfb96b374da7b3
xiangmin520 <516052955@qq.com>
2019-03-15 17:17:01 +0800

    Update UserRepository.java

--- src/main/java/com/xiami/blog/repository/UserRepository.java
+++ src/main/java/com/xiami/blog/repository/UserRepository.java
@@ -8,13 +8,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     /**
-     * 根据用户名分页查询用户列表。
-     *
+     * 根据用户姓名查询用户列表
+     * 
      * @param name
      * @param pageable
      * @return
      */
     Page<User> findByNameLike(String name, Pageable pageable);
 
+    /**
+     * 根据用户账号查询用户
+     *
+     * @param userName
+     * @return
+     */
     User findByUsername(String userName);
 }
