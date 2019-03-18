1583db7d0ee2196518a8b61e6bf8d83e05e0d367
xiangmin520 <516052955@qq.com>
2019-03-18 16:50:26 +0800

    Update User.java

--- src/main/java/com/xiami/blog/domain/User.java
+++ src/main/java/com/xiami/blog/domain/User.java
@@ -10,6 +10,13 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 /**
  * 用户实体
@@ -18,7 +25,7 @@ import java.io.Serializable;
 @NoArgsConstructor
 @Getter
 @Setter
-public class User implements Serializable {
+public class User implements UserDetails, Serializable {
 
     private static final long serialVersionUID = 1L;
 
@@ -50,6 +57,11 @@ public class User implements Serializable {
     @Column(length = 200)
     private String avatar; // 头像图片地址
 
+    @ManyToMany(cascade = CascadeType.DETACH, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_authority", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "authority_id", referencedColumnName = "id"))
+    private List<Authority> authorities;
+
     public User(String name, String email, String username,String password) {
         this.name = name;
         this.email = email;
@@ -61,5 +73,34 @@ public class User implements Serializable {
     public String toString() {
         return String.format("User[id=%d, username='%s', name='%s', email='%s', password='%s']", id, username, name, email,
                 password);
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        //  需将 List<Authority> 转成 List<SimpleGrantedAuthority>，否则前端拿不到角色列表名称
+        List<SimpleGrantedAuthority> simpleAuthorities = new ArrayList<>();
+        for(GrantedAuthority authority : this.authorities){
+            simpleAuthorities.add(new SimpleGrantedAuthority(authority.getAuthority()));
+        }
+        return simpleAuthorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
